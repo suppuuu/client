af994b23383c189db5b9a7a960da9281a9751bfb
supriya <supriyaannamdasu@gmail.com>
2025-02-01 15:54:01 +0530

    added another image

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
      <div>
       <img src="https://th.bing.com/th/id/OIF.qh4SwtayvKIMwOfBRxspaw?w=297&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"></img>
+      <img src="https://th.bing.com/th/id/OIF.qh4SwtayvKIMwOfBRxspaw?w=297&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"></img>
+
      </div>
     </div>
   );
