78a5cac85870c5ca0a874b379e9cba09471631ee
supriya <supriyaannamdasu@gmail.com>
2025-02-01 15:39:11 +0530

    added an image

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+      <img src="https://th.bing.com/th/id/OIF.qh4SwtayvKIMwOfBRxspaw?w=297&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"></img>
+     </div>
+    </div>
+  );
+}
+
+export default App;
